b4d614afb0f53b79c3993255f51f538fa4b20cac
Syed Asfar Ahmad Bukhari <syedasfar27@gmail.com>
2025-07-18 16:09:15 +0500

    Changed Hardcoded URLs

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,26 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://project-management-system-1emk-r76cnqb2b.vercel.app",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 
-// API Routes
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
@@ -20,11 +36,9 @@ app.use('/api/dashboard', require('./routes/dashboard'));
 app.use('/api/contact', require('./routes/contactRoutes'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 app.get('/', (req, res) => {
   res.send('API is working...');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
