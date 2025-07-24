c16055ca0357725dc698654a1eaf6f9673a06d0c
Syed Asfar Ahmad Bukhari <syedasfar27@gmail.com>
2025-07-24 16:00:18 +0500

    Change Role

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ exports.updateUserRole = async (req, res) => {
   }
 
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(id);
     if (!user) return res.status(404).json({ message: 'User not found' });
 
     user.role = newRole;
